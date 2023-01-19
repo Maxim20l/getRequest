1b52ecd8ffa9010447305222fd003880b6363b0e
Maxim Lyashenko <lyashenkokhpi2018@gmail.com>
2023-01-19 18:54:08 +0200

    errors

--- index.js
+++ index.js
@@ -6,23 +6,34 @@ const url = require('url');
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-    let urlReq = url.parse(req.url, true);
-    let selectFile = urlReq.path.slice(1);
-    const stream = fs.createReadStream(selectFile);
-    stream.pipe(res);
-    stream.on('error', (error) => {
-        if (error.code === 'ENOENT') {
-            res.statusCode = 404;
-            res.end();
-        } else {
-            res.statusCode = 400;
-            res.end();
-        }
-    })
+    try {
+        if (req.method === 'GET') {
+            let urlReq = url.parse(req.url, true);
+            let selectFile = urlReq.path.slice(1);
+            const stream = fs.createReadStream(selectFile);
+            stream.pipe(res);
+            stream.on('error', (error) => {
+                if (fs.existsSync(selectFile)) {
+                    res.statusCode = 404;
+                    res.end();
+                } else {
+                    res.statusCode = 500;
+                    res.end();
+                }
+            })
 
-    stream.on('aborted', () => {
-        stream.destroy()
-    })
+            stream.on('aborted', () => {
+                stream.destroy()
+            })
+        } else {
+            res.end()
+        }
+        
+
+    } catch (error) {
+        console.log(error);
+    }
+
 
 })
 
