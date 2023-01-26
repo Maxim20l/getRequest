06150e887e7fc9526d21ea5da73943449fff9a67
Maxim Lyashenko <lyashenkokhpi2018@gmail.com>
2023-01-26 20:41:17 +0200

    done

--- index.js
+++ index.js
@@ -1,42 +1,8 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
+const server = require('./server');
 
 
-const server = new http.Server();
+server.listen(3000, () => {
+    console.log('Server is listening on http://localhost:3000');
+});
 
-server.on('request', (req, res) => {
-    try {
-        if (req.method === 'GET') {
-            let urlReq = url.parse(req.url, true);
-            let selectFile = urlReq.path.slice(1);
-            const stream = fs.createReadStream(selectFile);
-            stream.pipe(res);
-            stream.on('error', (error) => {
-                if (fs.existsSync(selectFile)) {
-                    res.statusCode = 404;
-                    res.end();
-                } else {
-                    res.statusCode = 500;
-                    res.end();
-                }
-            })
-
-            stream.on('aborted', () => {
-                stream.destroy()
-            })
-        } else {
-            res.end()
-        }
-        
-
-    } catch (error) {
-        console.log(error);
-    }
-
-
-})
-
-
-server.listen(3000)
 
